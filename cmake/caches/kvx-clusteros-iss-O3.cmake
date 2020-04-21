3e66443b867c7e606272af9fbd6ce65709675ef7
Laurent Thévenoux <lthevenoux@kalray.eu>
2020-04-21 21:21:20 +0200

    Revert "[kvx] Temporarily disable hardware loops"
    Summary: This reverts commit 7b57a16ae2831dadfbef51769955571c8d53097b.
    
    Test Plan: n/a
    
    Reviewers: O81 llvm-coolidge!
    
    Differential Revision: https://phab.kalray.eu/D5586

--- cmake/caches/kvx-clusteros-iss-O3.cmake
+++ cmake/caches/kvx-clusteros-iss-O3.cmake
@@ -5,7 +5,7 @@
 # -Wl,--defsym=MPPA_COS_MASTER_CC=0             -> Run on Cluster 0
 # -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_SHIFT=0 -> Set 1 thread per core
 
-set(OPTFLAGS "${OPTFLAGS} -mllvm -disable-kvx-hwloops -O3 -Tmppadcos_ddr.ld -Wl,--defsym=USER_STACK_SIZE=128000 -Wl,--defsym=MPPA_COS_NB_CC=1 -Wl,--defsym=MPPA_COS_NB_CORES_SHIFT=0 -Wl,--defsym=MPPA_COS_MASTER_CC=0 -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_SHIFT=0")
+set(OPTFLAGS "${OPTFLAGS} -O3 -Tmppadcos_ddr.ld -Wl,--defsym=USER_STACK_SIZE=128000 -Wl,--defsym=MPPA_COS_NB_CC=1 -Wl,--defsym=MPPA_COS_NB_CORES_SHIFT=0 -Wl,--defsym=MPPA_COS_MASTER_CC=0 -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_SHIFT=0")
 
 set(CMAKE_C_FLAGS_RELEASE "${OPTFLAGS}" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELEASE "${OPTFLAGS}" CACHE STRING "")
