2cf0647189d28098bb0da4b042a68b013db755ba
jhascoet <jhascoet@kalray.eu>
2020-05-04 15:20:13 +0200

    [kvx] Update for Cluster renaming of config flags
    Summary:
    We udpate the configuration flags
    of ClusterOS.
    
    Ref T12816
    
    Test Plan: CI
    
    Reviewers: O81 llvm-coolidge!
    
    Maniphest Tasks: T12816
    
    Differential Revision: https://phab.kalray.eu/D5898

--- cmake/caches/kvx-clusteros-iss-O0-g.cmake
+++ cmake/caches/kvx-clusteros-iss-O0-g.cmake
@@ -1,11 +1,11 @@
 # -Tmppadcos_ddr.ld                             -> Use DDR
 # -Wl,--defsym=USER_STACK_SIZE=128000           -> Set stack size to 125Ko (SMEM)
 # -Wl,--defsym=MPPA_COS_NB_CC=1                 -> Use 1 cluster
-# -Wl,--defsym=MPPA_COS_NB_CORES_SHIFT=0        -> Use 1 core
+# -Wl,--defsym=MPPA_COS_NB_CORES_LOG2=0         -> Use 1 core
 # -Wl,--defsym=MPPA_COS_MASTER_CC=0             -> Run on Cluster 0
-# -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_SHIFT=0 -> Set 1 thread per core
+# -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_LOG2=0  -> Set 1 thread per core
 
-set(OPTFLAGS "${OPTFLAGS} -O0 -g -Tmppadcos_ddr.ld -Wl,--defsym=USER_STACK_SIZE=128000 -Wl,--defsym=MPPA_COS_NB_CC=1 -Wl,--defsym=MPPA_COS_NB_CORES_SHIFT=0 -Wl,--defsym=MPPA_COS_MASTER_CC=0 -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_SHIFT=0")
+set(OPTFLAGS "${OPTFLAGS} -O0 -g -Tmppadcos_ddr.ld -Wl,--defsym=USER_STACK_SIZE=128000 -Wl,--defsym=MPPA_COS_NB_CC=1 -Wl,--defsym=MPPA_COS_NB_CORES_LOG2=0 -Wl,--defsym=MPPA_COS_MASTER_CC=0 -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_LOG2=0")
 
 set(CMAKE_C_FLAGS_DEBUG "${OPTFLAGS}" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_DEBUG "${OPTFLAGS}" CACHE STRING "")
