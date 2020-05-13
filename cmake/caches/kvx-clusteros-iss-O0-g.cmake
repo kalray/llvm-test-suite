78d6a61247810526add143e56307015390c20c64
Laurent Thévenoux <lthevenoux@kalray.eu>
2020-05-13 12:32:07 +0200

    [kvx] Remove link flags from C/CXX_FLAGS in cmake files
    Test Plan: n/a
    
    Reviewers: O81 llvm-coolidge!
    
    Differential Revision: https://phab.kalray.eu/D6095

--- cmake/caches/kvx-clusteros-iss-O0-g.cmake
+++ cmake/caches/kvx-clusteros-iss-O0-g.cmake
@@ -5,8 +5,10 @@
 # -Wl,--defsym=MPPA_COS_MASTER_CC=0             -> Run on Cluster 0
 # -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_LOG2=0  -> Set 1 thread per core
 
-set(OPTFLAGS "${OPTFLAGS} -O0 -g -Tmppadcos_ddr.ld -Wl,--defsym=USER_STACK_SIZE=128000 -Wl,--defsym=MPPA_COS_NB_CC=1 -Wl,--defsym=MPPA_COS_NB_CORES_LOG2=0 -Wl,--defsym=MPPA_COS_MASTER_CC=0 -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_LOG2=0")
+set(OPTFLAGS "${OPTFLAGS} -O0 -g")
 
 set(CMAKE_C_FLAGS_DEBUG "${OPTFLAGS}" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_DEBUG "${OPTFLAGS}" CACHE STRING "")
+set(CMAKE_EXE_LINKER_FLAGS "-Tmppadcos_ddr.ld -Wl,--defsym=USER_STACK_SIZE=128000 -Wl,--defsym=MPPA_COS_NB_CC=1 -Wl,--defsym=MPPA_COS_NB_CORES_LOG2=0 -Wl,--defsym=MPPA_COS_MASTER_CC=0 -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_LOG2=0" CACHE STRING "")
+
 set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "")
