63bcb0ccc5bf52a8591a44274cd56c5bd7a729d9
Laurent Thévenoux <lthevenoux@kalray.eu>
2020-05-29 19:01:32 +0200

    [kvx] Enable SMALL_PROBLEM_SIZE
    Summary:
    Enable SMALL_PROBLEM_SIZE to reduce execution time of the testsuite.
    Refactor link options to improve readability.
    
    Test Plan: n/a
    
    Reviewers: O81 llvm-coolidge!
    
    Differential Revision: https://phab.kalray.eu/D6425

--- cmake/caches/kvx-clusteros-iss-O3.cmake
+++ cmake/caches/kvx-clusteros-iss-O3.cmake
@@ -1,14 +1,19 @@
-# -Tmppadcos_ddr.ld                             -> Use DDR
-# -Wl,--defsym=USER_STACK_SIZE=128000           -> Set stack size to 125Ko (SMEM)
-# -Wl,--defsym=MPPA_COS_NB_CC=1                 -> Use 1 cluster
-# -Wl,--defsym=MPPA_COS_NB_CORES_LOG2=0         -> Use 1 core
-# -Wl,--defsym=MPPA_COS_MASTER_CC=0             -> Run on Cluster 0
-# -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_LOG2=0  -> Set 1 thread per core
-
 set(OPTFLAGS "${OPTFLAGS} -O3")
 
 set(CMAKE_C_FLAGS_RELEASE "${OPTFLAGS}" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELEASE "${OPTFLAGS}" CACHE STRING "")
-set(CMAKE_EXE_LINKER_FLAGS "-Tmppadcos_ddr.ld -Wl,--defsym=USER_STACK_SIZE=128000 -Wl,--defsym=MPPA_COS_NB_CC=1 -Wl,--defsym=MPPA_COS_NB_CORES_LOG2=0 -Wl,--defsym=MPPA_COS_MASTER_CC=0 -Wl,--defsym=MPPA_COS_THREAD_PER_CORE_LOG2=0" CACHE STRING "")
+
+string(CONCAT linkerflags
+  "-Tmppadcos_ddr.ld "					# Use DDR
+  "-Wl,--defsym=USER_STACK_SIZE=0x20000 "		# Set stack size to 128KB (SMEM)
+  "-Wl,--defsym=MPPA_COS_NB_CC=1 "			# Use 1 Cluster (up to 5)
+  "-Wl,--defsym=MPPA_COS_MASTER_CC=0 "	 		# Run on Cluster 0
+  "-Wl,--defsym=MPPA_COS_NB_CORES_LOG2=0 "		# Use 1 Core (up to log2(16))
+  "-Wl,--defsym=MPPA_COS_THREAD_PER_CORE_LOG2=0"	# Set 1 thread per Core (up to log2(4))
+  )
+
+set(CMAKE_EXE_LINKER_FLAGS "${linkerflags}" CACHE STRING "")
 
 set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
+
+set(SMALL_PROBLEM_SIZE ON CACHE BOOL "")
