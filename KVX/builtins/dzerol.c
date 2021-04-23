2bcb68cc7439135012c0dec8c221b7878acbd11c
jhascoet <jhascoet@kalray.eu>
2021-04-23 13:41:38 +0200

    [kvx] Fix NULL pointer access for dzerol test
    Summary:
    We fix a NULL pointer
    access for the dzerol test.
    We also rebustify it by
    performing behavioural checks.
    
    Test Plan: LLVM
    
    Reviewers: pgmassas, O81 llvm-coolidge!
    
    Differential Revision: https://phab.kalray.eu/D12290

--- KVX/builtins/dzerol.c
+++ KVX/builtins/dzerol.c
@@ -1,8 +1,14 @@
 int main() {
 
 #ifdef __kvxarch_kv3_1
-  char *p = 0x0;
+  unsigned int i;
+  char p[64] __attribute__((aligned((64))));
+  memset(p, 1, sizeof(p));
   __builtin_kvx_dzerol(p);
+  for (i = 0; i < sizeof(p); i++) {
+    if (p[i] != 0)
+      return -1;
+  }
 #elif __kvxarch_kv3_2
   // No dzerol for kv3-2.
 #else
